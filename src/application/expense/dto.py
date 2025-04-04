55611c37472d260b1c84c6e1edb1e88e10bd59f9
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-04-04 02:06:43 +0200

    Created DTO

---
+++ src/application/expense/dto.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from pydantic import Field, condecimal
+
+from src.application.common.dto import DTO
+
+
+class ExpenseDTO(DTO):
+    id: int
+    name: str = Field(max_length=128)
+    amount_uah: condecimal(gt=0, max_digits=15, decimal_places=2)
+    amount_usd: condecimal(gt=0, max_digits=15, decimal_places=2)
+    created_at: datetime
+    updated_at: datetime
+
+
+class CreateExpenseRequest(DTO):
+    name: str = Field(max_length=128)
+    amount_uah: condecimal(gt=0, max_digits=15, decimal_places=2)
+
+
+class CreateExpenseDTO(CreateExpenseRequest):
+    amount_usd: condecimal(gt=0, max_digits=15, decimal_places=2)
+
+
+class EditExpenseRequest(DTO):
+    name: str | None = Field(default=None, max_length=128)
+    amount_uah: condecimal(gt=0, max_digits=15, decimal_places=2) | None = Field(default=None)
+
+
+class EditExpenseDTO(EditExpenseRequest):
+    amount_usd: condecimal(gt=0, max_digits=15, decimal_places=2) | None = Field(default=None)
