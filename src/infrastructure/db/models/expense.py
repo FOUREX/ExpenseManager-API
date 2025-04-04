fef72374f28ea2e15fab756102f836e73d98fa7f
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-04-04 02:06:28 +0200

    Created model ExpenseORM

---
+++ src/infrastructure/db/models/expense.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import String, DECIMAL, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.infrastructure.db.models.base import Base
+
+
+class ExpenseORM(Base):
+    __tablename__ = "Expense"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(length=128))
+    amount_uah: Mapped[float] = mapped_column(DECIMAL(15, 2))
+    amount_usd: Mapped[float] = mapped_column(DECIMAL(15, 2))
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
